98c3f42e56cde65f943d2e954c008ae8c0ce8a93
AntonioBr98 <92536025+AntonioBr98@users.noreply.github.com>
2022-03-30 15:25:01 +0200

    Add files via upload

---
+++ ClassifyingImagesWithVisionAndCoreML/App/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  BreastCancerDetect
+//
+//  Created by Antonio Braccolino on 29/03/22.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
